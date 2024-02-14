9eb069f0a066d152affc7726d403bda0a4dfc84c
8055SUBASH <11199a225@kanchiuniv.ac.in>
2024-02-14 22:41:06 +0530

    commited

--- server.js
+++ server.js
@@ -16,30 +16,85 @@ app.use(cors({
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser()); // Use cookie-parser middleware
 
-
-// -----live host-------
-
-// const connection = mysql.createConnection({
-
-//     host : "db4free.net",
-//     user : "vcentry",
-//     password : "test@123",
-//     database : "vcentry",    //travelix
-//     port : 3306
-// });
- 
-
-// ------Xampp----
-
 const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "vcentry"
+});
 
-    host : "db4free.net",
-    user : "vcentry",
-    password : "test@123",
-    database : "travelix",
-    port : 3306
+const transporter = nodemailer.createTransport({
+  service: 'gmail', // e.g., 'gmail', 'yahoo', etc.
+  auth: {
+      user: '11199a225@kanchiunv.ac.in',
+      pass: 'subash8055',
+  },
+});
+
+connection.connect((error) => {
+    if (error) {
+      throw error;
+    }
+    else {
+      console.log("MYSQL Server has been connected");
+    }
+  })
+
+// ---------------------------------------------------------------------------------------------------------
+// http://localhost:5000/api/read/review - this method is used in review frontend page
+// Method : GET
+
+app.get("/api/read/review", (request, response) => {
+  const sql_query = `SELECT * FROM vcentry_review`;
+  connection.query(sql_query, (error, result) => {
+    if (error) {
+      response.status(500).send(error);
+    }
+    else {
+      response.status(200).send(result);
+    }
+  })
+})  
+// ---------------------------------------------------------------------------------------------------------
+// http://localhost:5000/api/read/contact - this method is used in contact frontend page
+// Method : GET
+
+app.get("/api/read/contact", (request, response) => {
+  const sql_query = `SELECT * FROM vcentry_contact`;
+  connection.query(sql_query, (error, result) => {
+    if (error) {
+      response.status(500).send(error);
+    }
+    else {
+      response.status(200).send(result);
+    }
+  })
+})  
+
+// ---------------------------------------------------------------------------------------------------------
+// http://localhost:5000/api/create/review - this method is used in review frontend page
+// Method : POST
+
+app.post("/api/create/review", (request, response) => {
+
+    const sql_query = `INSERT INTO vcentry_review (Name, Email, Course, Message) 
+    VALUES ('${request.body.Name}', '${request.body.Email}', '${request.body.Course}', '${request.body.Message}')`;
+
+    connection.query(sql_query, (error, result) => {
+        if (error) {
+            response.status(500).send(error);
+        }
+        else {
+            response.status(200).send("Review has been updated");
+        }
+    })
 })
 
+// ---------------------------------------------------------------------------------------------------------
+// http://localhost:5000/api/submit/contact - this method is used in contact frontend page
+// Method : POST
+
+app.post("/api/create/contact", (request, response) => {
 
 
   const sql_query = `INSERT INTO vcentry_contact (username, useremail, userphoneno, usercourse, usermessage)
@@ -59,7 +114,7 @@ const connection = mysql.createConnection({
 //Method : DELETE
 
 app.delete("/api/delete/contact/:id", (request, response) => {
-  const sql_query = `DELETE FROM vcentry_contact WHERE id=${request.params.id}`;
+  const sql_query =` DELETE FROM vcentry_contact WHERE id=${request.params.id}`;
   connection.query(sql_query, (error, result) => {
     if(error){
       response.status(500).send(error);
@@ -70,8 +125,8 @@ app.delete("/api/delete/contact/:id", (request, response) => {
   })
 })
 
-const portNumber = process.env.PORT || 5000;
-server.listen(portNumber, () => {
-    console.log("server is running")
+// ---------------------------------------------------------------------------------------------------------
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log("Server is Running");
 })
-
