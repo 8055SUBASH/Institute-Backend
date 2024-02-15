8a678c74b193abfdf94ac647670ed112a98a944a
thilak <thilakct28@gmail.com>
2024-02-15 18:38:09 +0530

    detail,trend

--- server.js
+++ server.js
@@ -126,6 +126,117 @@ app.delete("/api/delete/contact/:id", (request, response) => {
 })
 
 // ---------------------------------------------------------------------------------------------------------
+
+
+
+// ---------------------------courses-API-POST---------------------------------
+
+
+
+app.post("/api/create/courses",(request,response) => {
+    const sql_query = `INSERT INTO vcentry_courses (coursesFiled , coursesName , coursesDetail , coursesImage) VALUES ('${request.body.coursesFiled}', '${request.body.coursesName}', '${request.body.coursesDetail}', '${request.body.coursesImage}')`;
+
+    connection.query(sql_query,(error, result) => {
+        if(error){
+            response.status(500).send(error);
+        }
+        else{
+            response.status(200).send("detail has been uploaded");
+        }
+    })
+});
+
+
+
+
+// --------------------------trend-courses-Api-POST----------------------------
+
+
+app.post("/api/create/trend",(request,response) => {
+    const sql_query = `INSERT INTO vcentry_trend_courses (coursesFiled , coursesName , coursesDetail , coursesImage) VALUES ('${request.body.coursesFiled}', '${request.body.coursesName}', '${request.body.coursesDetail}', '${request.body.coursesImage}')`;
+
+    connection.query(sql_query,(error, result) => {
+        if(error){
+            response.status(500).send(error);
+        }
+        else{
+            response.status(200).send("detail has been uploaded");
+        }
+    })
+});
+
+
+// --------------------------------courses-API-GET-------------------------------
+
+
+
+app.get("/api/list/courses", (request, response) => {
+    const sql_query = `SELECT * FROM vcentry_courses` ;
+    connection.query(sql_query,(error, result) => {
+        if(error){
+            response.status(500).send(error);
+        }
+        else{
+            response.status(200).send(result);
+        }
+    })
+});
+
+
+
+// --------------------------------trend-API-GET-------------------------------
+
+
+
+app.get("/api/list/trend", (request, response) => {
+    const sql_query = `SELECT * FROM vcentry_trend_courses` ;
+    connection.query(sql_query,(error, result) => {
+        if(error){
+            response.status(500).send(error);
+        }
+        else{
+            response.status(200).send(result);
+        }
+    })
+});
+
+
+// --------------------------------courses-API-Delete-------------------------------
+
+app.delete("/api/delete/courses/:id", (request, response) => {
+    const sql_query = `DELETE FROM vcentry_courses WHERE id=${request.params.id}`;
+    connection.query(sql_query,(error, result) => {
+        if(error){
+            response.status(500).send(error);
+        }
+        else{
+            response.status(200).send("Deleted Successfully");
+        }
+    })
+});
+
+
+
+
+// --------------------------------trend-API-Delete-------------------------------
+
+app.delete("/api/delete/trend/:id", (request, response) => {
+    
+    const sql_query = `DELETE FROM vcentry_trend_courses WHERE id=${request.params.id}`;
+    
+    connection.query(sql_query,(error, result) => {
+        if(error){
+            response.status(500).send(error);
+        }
+        else{
+            response.status(200).send("Deleted Successfully");
+        }
+    })
+});
+ 
+
+
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log("Server is Running");
